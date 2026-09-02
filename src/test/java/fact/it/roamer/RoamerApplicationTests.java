ebd405cb902e209f07ae088a5c1bd6d8d4c99bc0
xX-FLOWER-Xx <91901190+xX-FLOWER-Xx@users.noreply.github.com>
2026-09-02 22:09:32 +0200

    Initial commit

---
+++ src/test/java/fact/it/roamer/RoamerApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.roamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
